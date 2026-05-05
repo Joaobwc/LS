a4fb085eb4b204d4e499ef56d35fa9fe9d11405f
Joaobwc <joaoa7619@gmail.com>
2026-05-05 14:47:31 +0100

    Ficha 9 - Hooks de estado e eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import "./assets/styles/App.css";
 
 import { ControlPanel, Footer, Header, GamePanel } from "./components";
 
 function App() {
+  const [gameStarted, setGameStarted] = useState(false);
+  const [selectedLevel, setSelectdLevel] = useState("0");
+
+  const handleGameStart = () => {
+    if (gameStarted) setGameStarted(false);
+    else setGameStarted(true);
+  };
+  const handleLevelChange = (event) => {
+    const { value } = event.currentTarget;
+    //const value = event.currentTarget.value;
+
+    setSelectdLevel(value);
+  };
   return (
     <div id="container">
       <Header />
       <main>
-        <ControlPanel />
+        <ControlPanel
+          gameStarted={gameStarted}
+          onGameStart={handleGameStart}
+          onLevelChange={handleLevelChange}
+          selectedLevel={selectedLevel}
+        />
         <GamePanel />
       </main>
       <Footer />
--- src/components/control-panel/control-panel.component.jsx
+++ src/components/control-panel/control-panel.component.jsx
@@ -1,37 +1,51 @@
 import React from "react";
 import "./control-panel.css";
 
-function ControlPanel() {
+function ControlPanel(props) {
+  //passar valores de pai para o filho
+  const { gameStarted, onGameStart, onLevelChange, selectedLevel } = props;
+  const gameStartedClass = gameStarted ? " gameStarted" : "";
+
   return (
     <section id="panel-control">
       <h3 className="sr-only">Escolha do Nível</h3>
       <form className="form">
         <fieldset className="form-group">
           <label htmlFor="btLevel">Nível:</label>
-          <select id="btLevel">
+          <select
+            id="btLevel"
+            disabled={gameStarted}
+            defaultValue="0"
+            onChange={onLevelChange}
+          >
             <option value="0">Seleccione...</option>
             <option value="1">Básico (2x3)</option>
             <option value="2">Intermédio (3x4)</option>
             <option value="3">Avançado (4x5)</option>
           </select>
         </fieldset>
-        <button type="button" id="btPlay">
-          Iniciar Jogo
+        <button
+          type="button"
+          id="btPlay"
+          onClick={onGameStart}
+          disabled={selectedLevel === "0"}
+        >
+          {gameStarted ? "Terminar Jogo" : "Iniciar Jogo"}
         </button>
       </form>
       <div className="form-metadata">
         <p id="message" role="alert" className="hide">
           Clique em Iniciar o Jogo!
         </p>
-        <dl className="list-item left">
+        <dl className={"list-item left" + gameStartedClass}>
           <dt>Tempo de Jogo:</dt>
           <dd id="gameTime">0s</dd>
         </dl>
-        <dl className="list-item right">
+        <dl className={`list-item right${gameStartedClass}`}>
           <dt>Pontuação TOP:</dt>
           <dd id="pointsTop">0</dd>
         </dl>
-        <dl className="list-item left">
+        <dl className={`list-item left${gameStartedClass}`}>
           <dt>Pontuação:</dt>
           <dd id="points">0</dd>
         </dl>
